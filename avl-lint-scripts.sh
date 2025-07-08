358a1f4cde162d285da7a9031735b9cee29349f4
Arne Van Looveren <arne-vl@hotmail.be>
2025-07-08 16:44:12 +0200

    linting script

---
+++ avl-lint-scripts.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+###############################################
+#
+# Title:       avl-lint-scripts.sh
+# Author:      Arne Van Looveren
+# Description: Linting script for scripts repository
+# Usage:       avl-lint-scripts.sh
+#
+###############################################
+
+FAILURE=0
+
+while read -r file; do
+    echo "Linting $file"
+    if ! shellcheck --color=always -S warning "$file"; then
+        echo "Linting failed for $file"
+        FAILURE=1
+    fi
+    echo
+done < <(find . -type f ! -path "./.git/*" ! -path "./.github/*")
+
+if [[ $FAILURE -ne 0 ]]; then
+    echo "Linting failed for one or more files."
+    exit 1
+else
+    echo "All files passed linting."
+    exit 0
+fi
+
